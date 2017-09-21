c7e66ace62d206822a5635b91fd7281f5316f656
conmart <connor.a.martinelli@gmail.com>
2017-09-20 17:48:31 -0700

    Added logout and edit user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, except: [:index]
 
   get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy"
   post "/sessions", to: "sessions#create"
 
   # get '/users/new', to: 'users#new', as: 'new_user'
