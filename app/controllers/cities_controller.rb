c3d647739f7b8959c33365f3d2e5bae408616dc4
conmart <connor.a.martinelli@gmail.com>
2017-09-21 16:14:08 -0700

    Delete popup works

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,6 +2,7 @@ class CitiesController < ApplicationController
   def show
     @city = City.find_by_id(params[:id])
     @posts = @city.posts
+    @posts = @posts.order(created_at: :desc)
     last_city(@city)
   end
 end
