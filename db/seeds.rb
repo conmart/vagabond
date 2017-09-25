a0d285107aa33958a096c5f71ebfa2c1c40d68e7
conmart <connor.a.martinelli@gmail.com>
2017-09-25 10:43:58 -0700

    fixed edit user and added more cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,26 @@ City.destroy_all
   [
     {
       name: "San Francisco",
-      image: "https://images1.apartments.com/i2/hFgYoT0zCoCAdiUcUR22kOmdhv7VosGC2AghT5uI12w/110/san-francisco-ca-the-iconic-golden-gate-bridge.jpg"
+      image: "http://baycityguide.com/media/00PU000000KOXXxMAP/Golden-Gate-Bridge-Ft-Point-1500x87-30.jpg"
     },
     {
       name: "Gibraltar",
-      image: "http://i.telegraph.co.uk/multimedia/archive/02635/gibraltar_2635526k.jpg"
+      image: "https://a.travel-assets.com/findyours-php/viewfinder/images/res60/202000/202319-Rock-Of-Gibraltar.jpg"
     },
     {
       name: "London",
       image: "https://media.architecturaldigest.com/photos/56fd9d65ecd154e0329c1627/2:1/w_2560/london-travel-guide-lede.jpg"
+    },
+    {
+      name: "New York",
+      image: "http://mcny.org/sites/default/files/New-York-14-10-2489.jpg"
+    },
+    {
+      name: "Chicago",
+      image: "http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/1446655168/chicago-header-dg1115.jpg?itok=MqZFOaTi"
+    },
+    {
+      name: "Portland",
+      image: "https://www.visittheusa.com/sites/default/files/styles/hero_m_1300x700/public/images/hero_media_image/2016-11/Drone.__72%20DPI.jpg?itok=jhGikGPz"
     }
   ])
